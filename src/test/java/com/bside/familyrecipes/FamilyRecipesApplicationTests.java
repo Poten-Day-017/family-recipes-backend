f94fbae147cfa1a7c9355c9a5d2c86c55705d2e5
ghghtygh <ghghtygh@naver.com>
2024-01-29 22:30:46 +0900

    share project

---
+++ src/test/java/com/bside/familyrecipes/FamilyRecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bside.familyrecipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyRecipesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
