ebddfd3a234642c2aa285a45ac02d0dcf4727720
김주영 <gimjuyeong0620@gmail.com>
2024-02-02 17:10:27 +0900

    feat/#6-get-user-profile
    유저 닉네임 변경 API 개발

--- src/main/java/com/bside/familyrecipes/users/application/UserServiceImpl.java
+++ src/main/java/com/bside/familyrecipes/users/application/UserServiceImpl.java
@@ -2,11 +2,13 @@ package com.bside.familyrecipes.users.application;
 
 import com.bside.familyrecipes.common.message.ExceptionMessage;
 import com.bside.familyrecipes.users.domain.User;
+import com.bside.familyrecipes.users.dto.request.UserUpdateNicknameRequest;
 import com.bside.familyrecipes.users.dto.response.UserProfileResponse;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -16,6 +18,13 @@ public class UserServiceImpl implements UserService{
     public UserProfileResponse getUserProfile(Long userId) {
         val user = findUser(userId);
         return UserProfileResponse.of(user);
+    }
+
+    @Transactional
+    @Override
+    public void updateUserNickname(UserUpdateNicknameRequest request) {
+        val user = findUser(request.userId());
+        user.setUserNickname(request.nickname());
     }
 
     private User findUser(Long userId){
--- src/main/java/com/bside/familyrecipes/users/presentation/UserController.java
+++ src/main/java/com/bside/familyrecipes/users/presentation/UserController.java
@@ -2,14 +2,12 @@ package com.bside.familyrecipes.users.presentation;
 
 import com.bside.familyrecipes.users.application.UserService;
 import com.bside.familyrecipes.common.dto.response.ResponseDto;
+import com.bside.familyrecipes.users.dto.request.UserUpdateNicknameRequest;
 import com.bside.familyrecipes.users.dto.response.UserProfileResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -27,5 +25,14 @@ public class UserController {
     public ResponseEntity<UserProfileResponse> getUserProfile(@PathVariable Long userId){
         val response = userService.getUserProfile(userId);
         return ResponseDto.ok(response);
+    }
+
+    @PutMapping("/{userId}/nickname")
+    @Operation(description = "유저 닉네임을 변경한다.")
+    public String updateUserNickname(
+            @PathVariable Long userId,
+            @RequestBody UserUpdateNicknameRequest request){
+        userService.updateUserNickname(request);
+        return "success";
     }
 }
