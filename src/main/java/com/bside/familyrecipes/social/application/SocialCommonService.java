2d15e1086319868cb226de6c33641c3c612bcb36
김주영 <gimjuyeong0620@gmail.com>
2024-02-25 12:23:32 +0900

    feat: 로그인 수정 + relogin api 개발

--- src/main/java/com/bside/familyrecipes/social/application/SocialCommonService.java
+++ src/main/java/com/bside/familyrecipes/social/application/SocialCommonService.java
@@ -1,9 +1,10 @@
 package com.bside.familyrecipes.social.application;
 
+import com.bside.familyrecipes.social.dto.request.SocialLoginRequestDTO;
 import com.bside.familyrecipes.social.dto.response.SocialLoginResponseDTO;
 import com.bside.familyrecipes.social.enums.ProviderType;
 
 public interface SocialCommonService {
-    SocialLoginResponseDTO DaeDaeSonSonLogin(String thirdPartyUserId, ProviderType providerType, String deviceToken);
+    SocialLoginResponseDTO DaeDaeSonSonLogin(SocialLoginRequestDTO socialLoginRequestDTO);
 }
 
--- src/main/java/com/bside/familyrecipes/social/dto/request/SocialLoginRequestDTO.java
+++ src/main/java/com/bside/familyrecipes/social/dto/request/SocialLoginRequestDTO.java
@@ -3,8 +3,10 @@ package com.bside.familyrecipes.social.dto.request;
 import com.bside.familyrecipes.social.enums.ProviderType;
 
 public record SocialLoginRequestDTO(
-        String token,
-        ProviderType providerType,
-        String deviceToken
-) {
+    String name,
+    String email,
+    String picture,
+    ProviderType providerType,
+    String deviceToken
+){
 }
